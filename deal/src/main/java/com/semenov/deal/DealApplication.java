1d8ad90fb89c5543e6520f2c6633b11c31d4fff5
SemenovRustam <zeevvsss@yandex.ru>
2022-06-05 10:16:22 +0500

    Deal - service MVP_2

--- deal/src/main/java/com/semenov/deal/DealApplication.java
+++ deal/src/main/java/com/semenov/deal/DealApplication.java
@@ -1,4 +1,4 @@
-package com.semenov.demo;
+package com.semenov.deal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- deal/src/test/java/com/semenov/deal/DealApplicationTests.java
+++ deal/src/test/java/com/semenov/deal/DealApplicationTests.java
@@ -1,4 +1,4 @@
-package com.semenov.demo;
+package com.semenov.deal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
