c20f0a28eb8fa731036a518b0f2e898ba2e5034e
SemenovRustam <zeevvsss@yandex.ru>
2022-06-12 19:45:06 +0500

    folder reorganization

--- deal/src/main/java/com/semenov/deal/exceptionhandling/DealAppException.java
+++ deal/src/main/java/com/semenov/deal/exceptionhandling/DealAppException.java
@@ -1,6 +1,6 @@
 package com.semenov.deal.exceptionhandling;
 
-public class DealAppException extends  RuntimeException{
+public class DealAppException extends RuntimeException {
     public DealAppException(String message) {
         super(message);
     }
