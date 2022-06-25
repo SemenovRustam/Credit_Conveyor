69e404f233a7992c3074206ccda969bf0b47dbfe
SemenovRustam <zeevvsss@yandex.ru>
2022-06-25 23:23:26 +0500

    mvp_4

--- dossier/src/main/java/com/semenov/dossier/DossierApplication.java
+++ dossier/src/main/java/com/semenov/dossier/DossierApplication.java
@@ -2,10 +2,12 @@ package com.semenov.dossier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableKafka
+@PropertySource("classpath:prop.properties")
 public class DossierApplication {
 
     public static void main(String[] args) {
