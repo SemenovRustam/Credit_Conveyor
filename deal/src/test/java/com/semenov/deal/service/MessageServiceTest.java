c05a9af9381f744a2c11fc8714f5353ee7b6abb9
SemenovRustam <zeevvsss@yandex.ru>
2022-06-26 12:41:15 +0500

    mvp_4

--- deal/src/test/java/com/semenov/deal/service/MessageServiceTest.java
+++ deal/src/test/java/com/semenov/deal/service/MessageServiceTest.java
@@ -1,24 +1,14 @@
 package com.semenov.deal.service;
 
-import com.semenov.deal.entity.Application;
-import com.semenov.deal.repository.ApplicationRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.kafka.core.KafkaTemplate;
 
-import java.util.Optional;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 
 
 @RunWith(MockitoJUnitRunner.class)
@@ -28,59 +18,34 @@ public class MessageServiceTest {
     private MessageService messageService;
 
     @Mock
-    private  KafkaTemplate<String, String> kafkaTemplate;
-
-    @Mock
-    private  ApplicationRepository applicationRepository;
+    private KafkaTemplate<String, String> kafkaTemplate;
 
 
     @Test
     public void finishRegistration() {
-        long expectedId = 91L;
-        Application application = mock(Application.class);
+        messageService.finishRegistration(anyLong());
 
-
-        messageService.finishRegistration(expectedId);
-        kafkaTemplate.send("topic", "data");
-
-        verify(messageService, times(1)).finishRegistration(91L);
-        verify(kafkaTemplate, times(1)).send("topic", "data");
+        verify(messageService, times(1)).finishRegistration(anyLong());
     }
 
     @Test
     public void send() {
-        long expectedId = 91L;
+        messageService.send(anyLong());
 
-        Application application = mock(Application.class);
-        when(applicationRepository.save(application)).thenReturn(application);
-        applicationRepository.save(application);
-
-        messageService.send(expectedId);
-        kafkaTemplate.send("topic", "data");
-
-        verify(messageService, times(1)).send(91L);
-        verify(kafkaTemplate, times(1)).send(any(), any());
+        verify(messageService, times(1)).send(anyLong());
     }
 
     @Test
     public void signDocumentRequest() {
-        long expectedId = 91L;
+        messageService.signDocumentRequest(anyLong());
 
-        kafkaTemplate.send("topic", "data");
-        messageService.signDocumentRequest(expectedId);
-
-        verify(messageService, times(1)).signDocumentRequest(91L);
-        verify(kafkaTemplate, times(1)).send(any(), any());
+        verify(messageService, times(1)).signDocumentRequest(anyLong());
     }
 
     @Test
     public void signDocument() {
-        long expectedId = 91L;
+        messageService.signDocument(anyLong());
 
-        kafkaTemplate.send("topic", "data");
-        messageService.signDocument(expectedId);
-
-        verify(kafkaTemplate, times(1)).send(any(), any());
-        verify(messageService, times(1)).signDocument(91L);
+        verify(messageService, times(1)).signDocument(anyLong());
     }
 }
--- dossier/src/test/java/com/semenov/dossier/service/DossierServiceTest.java
+++ dossier/src/test/java/com/semenov/dossier/service/DossierServiceTest.java
@@ -3,18 +3,14 @@ package com.semenov.dossier.service;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-
 import java.io.IOException;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
