b7bb87d9dce00e0c46ed78c0c20b0f0e61a9cbc3
SemenovRustam <zeevvsss@yandex.ru>
2022-09-18 20:28:18 +0700

    fix swagger config

--- deal/src/main/java/com/semenov/deal/config/SwaggerConfig.java
+++ deal/src/main/java/com/semenov/deal/config/SwaggerConfig.java
@@ -2,13 +2,11 @@ package com.semenov.deal.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
